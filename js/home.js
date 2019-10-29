5b2da79222657dfb3cd43c5c245075d5f7844073
Joaquin Mancini <andrx_it@hotmail.com>
2019-10-29 14:49:45 -0300

    Home view and MDB_files added

---
+++ js/home.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let btns = document.getElementsByClassName("nav-item");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function () {
+            let current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+
+    function load(urlPartial) {
+        fetch(urlPartial).then(
+            function (response) {
+                response.text().then(
+                    function (texto) {
+                        document.querySelector('.cuerpo').innerHTML = texto;
+                    }
+                )
+            }
+        )
+    }
+    load('html/general.html')
+    let jsBtn = document.getElementsByClassName("js-btn");
+    jsBtn[0].addEventListener('click', e => load('html/general.html'));    
+    jsBtn[1].addEventListener('click', e => load('html/general.html'));
+})
