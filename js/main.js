ae9f99df74458e3ca19ad2deb548f9e0c4b3e491
Mariano Nieves <marianonieves22@gmail.com>
2019-11-01 12:57:48 -0300

    Union of projects in one

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
-'use strict'
-document.addEventListener('DOMContentLoaded', function(){
-    function load(urlPartial){
-        fetch(urlPartial).then(
-            function(response){
-                response.text().then(
-                    function(texto){
-                        document.querySelector('.cuerpo').innerHTML=texto;
-                    }
-                )
-            }
-        )
-    }
-    
-    
-    load('html/suscribirse3.html');
-    let suscribirse1 = document.querySelector(".btn-suscribirse");
-    suscribirse1.addEventListener('click', e=> load('html/suscribirse1.html'));
-})
+"use strict"
+let suscription1 = 'html/suscription1.html'
+let suscription2 = 'html/suscription2.html'
+let suscription3 = 'html/suscription3.html'
+let inicio = 'html/inicio.html'
+let start_sesion = 'html/logging.html'
+
+firstLoad();
+
+function firstLoad() {
+  fetch(inicio).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+
+function suscription_first_step() {
+  fetch(suscription1).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+
+function suscription_second_step() {
+  fetch(suscription2).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+
+function suscription_third_step() {
+  fetch(suscription3).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+function logging() {
+  fetch(start_sesion).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+function home(){
+  window.location.href = 'home.html';
+}
+
+
