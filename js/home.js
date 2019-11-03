3290e5963a7d737757e91cb4b2ba856b642835ce
Joaquin Mancini <andrx_it@hotmail.com>
2019-11-03 20:43:45 -0300

    Auto stash before merge of "gh-pages" and "origin/gh-pages"

--- js/home.js
+++ js/home.js
@@ -1,13 +1,7 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function () {
     let btns = document.getElementsByClassName("nav-item");
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function () {
-            let current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        });
-    }
+    let nav = document.getElementsByClassName("navbar-collapse");
 
     function load(urlPartial) {
         fetch(urlPartial).then(
@@ -20,11 +14,35 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         )
     }
-    let hmBtn=document.getElementsByClassName("burguer");
-    
-    load('html/general.html')
+    load('html/general.html');
     let jsBtn = document.getElementsByClassName("js-btn");
-    jsBtn[0].addEventListener('click', e => load('html/general.html'));    
+    jsBtn[0].addEventListener('click', e => load('html/general.html'));
     jsBtn[1].addEventListener('click', e => load('html/general.html'));
+
+    let menuBtn = document.getElementsByClassName("navbar-toggler");
+    let icon = document.getElementsByClassName("material-icons");
+    menuBtn[0].addEventListener('click', function () {
+        if (menuBtn[0].classList.contains("burger")) {
+            menuBtn[0].classList.remove("burger");
+            icon[0].innerHTML = "menu";
+        } else {            
+            icon[0].innerHTML = "close";
+            menuBtn[0].classList.add("burger");
+        }
+    });
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function () {
+            let current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+            if (nav[0].classList.contains("show")) {
+                nav[0].classList.remove("show");
+                menuBtn[0].classList.remove("burger");
+                icon[0].innerHTML = "menu";
+            }
+        });
+    }
+
 })
 
