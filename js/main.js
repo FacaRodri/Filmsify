09459d345bcfbde529de07b3a662f5554eb79a67
Joaquin Mancini <andrx_it@hotmail.com>
2019-11-05 08:27:45 -0300

    Fourth suscription step added(confirm info)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 let suscription1 = 'html/suscription1.html'
 let suscription2 = 'html/suscription2.html'
 let suscription3 = 'html/suscription3.html'
+let suscription4 = 'html/suscription4.html'
 let inicio = 'html/inicio.html'
 let start_sesion = 'html/logging.html'
 
@@ -45,6 +46,17 @@ function suscription_second_step() {
 
 function suscription_third_step() {
   fetch(suscription3).then(
+      function (r) {
+        r.text().then(function (r) {
+          document.querySelector(".partial").innerHTML = r;
+        })
+      })
+    .catch(function (response) {
+      document.querySelector(".partial").innerHTML = "<h1>No es posible acceder en este momento, intente mas tarde,</h1>"
+    });
+}
+function suscription_forth_step() {
+  fetch(suscription4).then(
       function (r) {
         r.text().then(function (r) {
           document.querySelector(".partial").innerHTML = r;
